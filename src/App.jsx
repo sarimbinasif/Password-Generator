8d93c2437ac18d869ce7183c0b12b9ee6f0ad943
Sarim Bin Asif <sarimbinasif12345@gmail.com>
2024-07-07 18:25:00 +0500

    copy password feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,57 @@
-import './App.css'
-import { useState,  } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 function App() {
   const [length, setLength] = useState(8)
-  const [numberAllowed, setNumberAllowed] = useState(false)  
-  const [characterAllowed, setCharacterAllowed] = useState(false)
-  // const [password, setPassword] = useState("")
-  // 
-  
-  // const passwordGenerator = useCallback(() => {
-  //   let password = ""
-  //   let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-  //   if (numberAllowed) str += "0123456789"
-  //   if (characterAllowed) str += "!@#$%^&*-_+=[]{}~`"
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
 
-  //   for (let i = 1; i <= length; i++) {
-  //     let char = Math.floor(Math.random() * str.length + 1)
-  //     password += str.charAt(char)
+  //useRef hook
+  const passwordRef = useRef(null)
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" //simple password
+    if (numberAllowed) str += "0123456789"//simple+num
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"//simple+chars
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
       
-  //   }
+    }
 
-  //   setPassword(password)
+    setPassword(pass)
 
 
-  // }, [length, numberAllowed, characterAllowed, setPassword])
+  }, [length, numberAllowed, charAllowed, setPassword])
 
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 101);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
-    <>
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-yellow-500">
-      <h1 className='text-grey text-center my-3'>Password Generator</h1>
+    
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-yellow-500">
+      <h3 className='text-yellow text-center my-3'>Password generator</h3>
     <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
             type="text"
-            // value={password}
+            value={password}
             className="outline-none w-full py-1 px-3"
             placeholder="Password"
             readOnly
-            // ref={passwordRef}
+            ref={passwordRef}
         />
         <button
-        // onClick={copyPasswordToClipboard}
-        className='outline-none bg-green-700 text-white px-3 py-0.5 shrink-0'
-        >Copy Text</button>
+        onClick={copyPasswordToClipboard}
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >copy</button>
         
     </div>
     <div className='flex text-sm gap-x-2'>
@@ -70,17 +80,17 @@ function App() {
       <div className="flex items-center gap-x-1">
           <input
               type="checkbox"
-              defaultChecked={characterAllowed}
+              defaultChecked={charAllowed}
               id="characterInput"
               onChange={() => {
-                  setCharacterAllowed((prev) => !prev )
+                  setCharAllowed((prev) => !prev )
               }}
           />
           <label htmlFor="characterInput">Characters</label>
       </div>
     </div>
 </div>
-    </>
+    
   )
 }
 
